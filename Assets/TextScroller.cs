4bbd6732bfcb36a0b791d37f95ec8bf4f968a47d
Ggm-gijoo <gijoo1012@gmail.com>
2022-09-02 23:42:46 +0900

    가사 출력 시스템 구현

--- Assets/TextScroller.cs
+++ Assets/TextScroller.cs
@@ -1,13 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class TextScroller : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public LyricsDataManager lyricsDataManager;
+    public TextMeshProUGUI lyricsText;
+    public AudioSource yanTheme;
+    public int lyricsId = 0;
+    public int lyricsIndex = 0;
+
     void Start()
     {
         Invoke("OnText", 17.5f);
+        yanTheme.Play();
+        lyricsText.text = "";
     }
 
     void Update()
@@ -16,12 +24,29 @@ public class TextScroller : MonoBehaviour
 
     public void OnText()
     {
+        Debug.Log("실행");
         StartCoroutine(TextScroll());
     }
 
     private IEnumerator TextScroll()
     {
-        yield return new WaitForSeconds(4f);
+        Debug.Log("실행2");
+        string lyricsData = lyricsDataManager.GetLyrics(lyricsId, lyricsIndex);
+        while (lyricsId <= 2)
+        {
+            lyricsData = lyricsDataManager.GetLyrics(lyricsId, lyricsIndex);
+            if (lyricsData == null)
+            {
+                lyricsIndex = 0;
+                lyricsId++;
+                if (lyricsId >= 3)
+                    break;
+            }
+            lyricsData = lyricsDataManager.GetLyrics(lyricsId, lyricsIndex);
+            lyricsText.text = lyricsData;
+            lyricsIndex++;
+            yield return new WaitForSeconds((60f/119f) * 4f);
+        }
 
     }
 }
