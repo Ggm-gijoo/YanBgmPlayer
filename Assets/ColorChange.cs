8e196bc29c8e6f58357daa4a74c0bd120157aeed
Ggm-gijoo <gijoo1012@gmail.com>
2022-09-03 01:04:26 +0900

    작업 완료

--- Assets/ColorChange.cs
+++ Assets/ColorChange.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+using TMPro;
 
 public class ColorChange : MonoBehaviour
 {
@@ -11,7 +12,10 @@ public class ColorChange : MonoBehaviour
     SpriteRenderer colorBackgroundImage;
     [SerializeField]
     VolumeProfile volumeProfile;
+    [SerializeField]
+    TextMeshProUGUI textLyrics;
     Bloom bloom;
+    Gradient gradient;
     float angle = 0f;
     [SerializeField]
     float bloonInt = 0f;
@@ -31,8 +35,9 @@ public class ColorChange : MonoBehaviour
             angle -= 1f;
         }
         bloom.intensity.Override((float)((0.5 * Mathf.Sin(volumeInt - Mathf.PI/2)) + 0.5f) * bloonInt + 5f);
-        colorCube.color = Color.HSVToRGB(angle,0.3f,1f);
-        colorBackgroundImage.color = Color.HSVToRGB(angle, 0.2f, 1f);
+        colorCube.color = Color.HSVToRGB(angle,0.2f,1f);
+        colorBackgroundImage.color = Color.HSVToRGB(angle, 0.3f, 1f);
+        textLyrics.color = Color.HSVToRGB(angle, 0.2f, 1f);
         Camera.main.backgroundColor = Color.HSVToRGB(angle, 0.3f, 0.2f);
     }
 }
